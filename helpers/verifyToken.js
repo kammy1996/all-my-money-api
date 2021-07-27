caab7fd39012fd92ba02c7137311daf7be7537bb
kammy <kamranmemon25@gmail.com>
2021-07-27 19:11:08 +0530

    verify token function added and test apis

---
+++ helpers/verifyToken.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+verifyToken = (req,res,next) => {
+  const token = req.headers.authorization;
+  if(!token) return res.status(401).send(`user Token not Provided`);
+  try {
+    const verified = jwt.verify(token,process.env.JWT_SECRET);
+    req.user = verified;
+    next()
+  }
+  catch(err) {
+    res.status(400).send(`Access Denied`)
+  }
+}
+
+module.exports = verifyToken;
--- routes/records.js
+++ routes/records.js
@@ -1,7 +1,7 @@
 const router = require(`express`).Router();
-
+const verifyToken = require('../helpers/verifyToken');
 const recordController = require('../controllers/records')
 
-router.post('/account', recordController.addAccount)
+router.post('/account', verifyToken, recordController.addAccount)
 
 module.exports = router;
